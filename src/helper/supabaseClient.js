802e05f440124a2d699926a472d7b5ab4e0c408d
Oral <valodiyasmirnov91@gmail.com>
2025-10-22 16:16:38 +0500

    SUPABASEKEY_3

--- src/helper/supabaseClient.js
+++ src/helper/supabaseClient.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { createClient } from '@supabase/supabase-js';
 
 // Инициализация Supabase
-const supabaseUrl = import.meta.env.VITE_SUPABASEURL;
-const supabaseKey = import.meta.env.VITE_SUPABASEKEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-console.log('URL:', import.meta.env.VITE_SUPABASEURL);
-console.log('URL:', import.meta.env.VITE_SUPABASEKEY);
+console.log('URL:', import.meta.env.VITE_SUPABASE_URL);
+console.log('URL:', import.meta.env.VITE_SUPABASE_ANON_KEY);
 
 export default supabase;
