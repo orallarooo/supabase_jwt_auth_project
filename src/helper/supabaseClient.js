1be2e8c32ada44be1297b41cfcc3fe10fa4ce01c
Oral <valodiyasmirnov91@gmail.com>
2025-10-22 16:09:51 +0500

    SUPABASEKEY_2

--- src/helper/supabaseClient.js
+++ src/helper/supabaseClient.js
@@ -6,7 +6,7 @@ const supabaseUrl = import.meta.env.VITE_SUPABASEURL;
 const supabaseKey = import.meta.env.VITE_SUPABASEKEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-console.log('URL:', import.meta.env.VITE_supabaseUrl);
-console.log('URL:', import.meta.env.VITE_supabaseKey);
+console.log('URL:', import.meta.env.VITE_SUPABASEURL);
+console.log('URL:', import.meta.env.VITE_SUPABASEKEY);
 
 export default supabase;
